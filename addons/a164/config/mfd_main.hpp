60d772ce5695d64c104c7d9deb3e84b69bbe696a
Donov <donovancunningham@rocektmail.com>
2021-09-28 23:46:20 -0400

    Reproducing Problem
    Reproducing problem that occurred during merge from vanilla land vehicles (uncommitted 1 line)

--- addons/a164/config/mfd_main.hpp
+++ addons/a164/config/mfd_main.hpp
@@ -38,6 +38,6 @@ class Draw {
 	class page_nav {
 		//TODO: Radar Sensor Setup
 		condition = COND_ISNUMBER(MFD_PAGE_INDEX,MFD_PAGE_NAV);
-		//#include "mfd\page_nav_draw.hpp"
+		#include "mfd\page_nav_draw.hpp"
 	};
 };
